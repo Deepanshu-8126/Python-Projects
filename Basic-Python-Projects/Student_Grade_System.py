d515a7092b489c83a1ce2578ff2f45b58aa69e2b
Deepanshu <deepanshukapri4@gmail.com>
2025-11-01 09:15:00 +0530

    Add student grade system based on marks

---
+++ Basic-Python-Projects/Student_Grade_System.py
@@ -0,0 +1,13 @@
+name = input("Enter student name: ")
+marks = float(input("Enter marks (out of 100): "))
+
+if marks >= 90:
+    grade = "A"
+elif marks >= 75:
+    grade = "B"
+elif marks >= 60:
+    grade = "C"
+elif marks >= 40:
+    grade = "D"
+else:
+    grade = "Fail"
